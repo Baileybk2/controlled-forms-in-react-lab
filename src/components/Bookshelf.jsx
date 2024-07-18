07ead1af96e36cc948c564883c244e2a8243f634
BaileyBeck <baileybk2@gmail.com>
2024-07-18 12:33:22 -0400

    added event handlers, mapped through books array, created bookCard, added own styling

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,27 +1,58 @@
 import { useState } from "react";
 
 const Bookshelf = () => {
+  // state variables
   const [books, setBooks] = useState([]);
   const [newBook, setNewBook] = useState({
     title: "",
     author: "",
   });
 
+  // event handlers
+  const handleInputChange = (event) => {
+    setNewBook({ ...newBook, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setBooks([...books, newBook]);
+    setNewBook({ title: "", author: "" });
+  };
+
+  // return statement
   return (
     <>
       <div className="bookshelfDiv">
         <div className="formDiv">
           <h3>Add a Book</h3>
-          <label htmlFor="title">Title: </label>
-          <input id="title" name="title" value="title" />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="title">Title: </label>
+            <input
+              id="title"
+              name="title"
+              value={newBook.title}
+              onChange={handleInputChange}
+            />
 
-          <label htmlFor="author">Author: </label>
-          <input id="author" name="author" value="author" />
-          <button type="submit">Add book</button>
+            <label htmlFor="author">Author: </label>
+            <input
+              id="author"
+              name="author"
+              value={newBook.author}
+              onChange={handleInputChange}
+            />
+            <button type="submit">Submit book</button>
+          </form>
         </div>
-        <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+        <div className="bookCardsDiv">
+          {books.map((book, index) => (
+            <div key={index} className="bookCard">
+              <h4>{book.title}</h4>
+              <p>{book.author}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      ;
     </>
   );
 };
